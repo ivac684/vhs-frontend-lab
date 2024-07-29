c94724e679f6c041c881df202ac848f60ddf09d5
ivac684 <80286288+ivac684@users.noreply.github.com>
2024-07-29 23:15:44 +0200

    Update api/movie/[id].tsx

--- vhs-frontend/src/pages/api/movie/[id].tsx
+++ vhs-frontend/src/pages/api/movie/[id].tsx
@@ -6,7 +6,6 @@ const deleteMovieById = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
-  // Handle preflight request
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
